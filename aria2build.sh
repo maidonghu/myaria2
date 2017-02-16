b828081e812ce200b0d61eadeaab5c6f3203cd1d
maidonghu <maidonghu@users.noreply.github.com>
2017-02-15 16:36:37 -0800

    Update aria2build.sh

--- aria2build.sh
+++ aria2build.sh
@@ -1,12 +1,17 @@
-git clone https://github.com/aria2/aria2.git
-apt install libgnutls-dev nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config
-apt install libcppunit-dev autoconf automake autotools-dev autopoint libtool
-cd aria2/
-autoreconf -i
-./configure
-make -j8
-checkinstall
-make install
-make clean
-make distclean
-cd ..
+#!/bin/sh
+echo -e '\r' | add-apt-repository ppa:jonathonf/ffmpeg-3
+apt update && apt install ffmpeg aria2
+
+
+#git clone https://github.com/aria2/aria2.git
+#apt install libgnutls-dev nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config
+#apt install libcppunit-dev autoconf automake autotools-dev autopoint libtool
+#cd aria2/
+#autoreconf -i
+#./configure
+#make -j8
+#checkinstall
+#make install
+#make clean
+#make distclean
+#cd ..
