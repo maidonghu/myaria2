3ad9b8916761a0ea0828466420524873714507e5
maidonghu <maidonghu@users.noreply.github.com>
2017-02-15 16:44:00 -0800

    Update aria2build.sh

--- aria2build.sh
+++ aria2build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 echo -e '\r' | add-apt-repository ppa:jonathonf/ffmpeg-3
-apt update && apt install ffmpeg aria2
-
+apt update && apt install ffmpeg aria2 -y
+wget https://github.com/maidonghu/myaria2/blob/master/aria2_1.31.0-1_amd64.deb
+dpkg -i aria2_1.31.0-1_amd64.deb
+wget https://raw.githubusercontent.com/maidonghu/myaria2/master/aria2.conf
+mv aria2.conf /usr/local/etc/aria2.conf
+aria2c --conf-path=/usr/local/etc/aria2.conf
 
 #git clone https://github.com/aria2/aria2.git
 #apt install libgnutls-dev nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config
